e032e1fcd29930e6f0ed44262a91bbb2eac52514
Cristian Teran Juarez <82991437+NEARR27@users.noreply.github.com>
2023-11-22 07:25:12 -0600

    Update tensor_client.py

--- tensor_client.py
+++ tensor_client.py
@@ -1,15 +1,15 @@
 import requests
 
-# URL del servidor que aloja el modelo de predicción del precio de venta
-SERVER_URL = 'http://172.17.0.3:8501/v1/models/linear-model:predict'
+
+SERVER_URL = 'https://linear-model-service-nearr27.cloud.okteto.net/v1/models/linear-model:predict'
 
 def main():
-    # Datos de entrada simulando diferentes costos de producción
+    
     predict_request = '{"instances" : [ [20], [40], [60], [80], [100] ]}'
     
-    # Enviar la solicitud y obtener la predicción
+    
     response = requests.post(SERVER_URL, data=predict_request)
-    response.raise_for_status()  # Verificar si la respuesta contiene un error
+    response.raise_for_status()  
     prediction = response.json()
     print("Predicciones de precio de venta para diferentes costos de producción:", prediction)
 
